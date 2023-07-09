8f494f1e456ea6f52e93cb66cf4dace74e180159
Bryan Kenneth <bryankenneth0309@gmail.com>
2023-07-09 10:06:28 +0400

    fix(extensions): Fix html to string conversion

--- BryanMandiriAssesment/Resources/Extensions/Extensions.swift
+++ BryanMandiriAssesment/Resources/Extensions/Extensions.swift
@@ -12,11 +12,12 @@ import SnapKit
 extension String {
     var htmlToAttributedString: NSAttributedString? {
         guard let data = data(using: .utf8) else { return nil }
-        do {
-            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
-        } catch {
-            return nil
+        
+        if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
+              return attributedString
         }
+        return NSAttributedString(string: "")
+        
     }
     var htmlToString: String {
         return htmlToAttributedString?.string ?? ""
